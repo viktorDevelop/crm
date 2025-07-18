bc70a34f10a2139b155d4ac813357c78c6a88b6d
Ваше Имя <you@example.com>
2025-07-18 15:16:12 +0300

    fix:test

--- index.php
+++ index.php
@@ -8,21 +8,12 @@ include $_SERVER['DOCUMENT_ROOT'].'/init.php';
 //\core\Application::run();
 
 
-//$blade = new \core\SimpleBlade( '/views',  '/cache');
-//
-//echo $blade->render('pages/home', [
-//    'title' => 'Home Page',
-//    'name' => 'John Doe',
-//    'isAdmin' => true,
-//    'items' => ['Apple', 'Banana', 'Orange']
-//]);
-
 
 class View
 {
     private $data = [];
     private static $instance;
-
+    public $title;
     private function __construct() {}
 
     public static function getInstance()
@@ -45,14 +36,12 @@ class View
      * @return false|string
      *
      */
-    public function render($view ='')
+    public function render($view ='',$data=[])
     {
-//        foreach ($this->data as $key => $value) {
-//            $$key = $value;
-//        }
         extract($this->data, EXTR_SKIP);
+        extract( $data, EXTR_SKIP);
         ob_start();
-        $path = $_SERVER['DOCUMENT_ROOT'].'/views/'.$view.'.php';
+         $path = $_SERVER['DOCUMENT_ROOT'].'/views/'.$view.'.php';
 
         if (file_exists($path)) {
             include $path;
@@ -62,20 +51,75 @@ class View
 
         $content = ob_get_contents();
         ob_clean();
-
-        $content = preg_replace(
-            '/@section\(\'(.+?)\'\)/',
-            '<?php $this->include("$1"); ?>',$content);
         return $content;
     }
 
-    public function include($path)
+
+    public function include($path, $data = [])
     {
-        echo $this->render($path);
+        echo $this->render($path,$data);
+    }
+
+    public function page($view,$data = [])
+    {
+        $this->data[$view.'Data'] = $data;
+        $this->data['title'] = $this->title;
+        $this->data['page'] = 'home';
     }
 
 }
 
-$v = View::getInstance();
-$v->title = 'ttt title';
-echo $v->render('layouts/main');
+class Autorization
+{
+    public function checkAuth()
+    {
+        return isset($_SESSION['token']) ;
+    }
+
+    public function autorizate()
+    {
+        $token = time().'.'.md5($this->login);
+    }
+}
+
+
+
+class BaseController
+{
+    public function beforeExecute()
+    {
+
+    }
+
+    public function afterExecute()
+    {
+        $v = View::getInstance();
+        echo $v->render('layouts/main');
+    }
+
+}
+
+class PageController extends BaseController
+{
+    public function execute()
+    {
+        $v = View::getInstance();
+
+        $v->title = 'about Page';
+        $arResult = [
+            'title' => 'about Page',
+            'isAuth' => false,
+            'items' => ['Apple', 'Banana', 'Orange']
+        ];
+
+        $v->page('about',$arResult);
+    }
+
+
+}
+
+$c = new PageController();
+$c->beforeExecute();
+$c->execute();;
+$c->afterExecute();
+
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?=$title?></title>
+    <title><?=$title;?></title>
 </head>
 <body>
 
 <?
 /** @var $this \View  */
+
+
 $this->include('partials/header'); ?>
 
-<?$this->include('');?>
+<? $this->include('pages/'.$page); ?>
+
+<? $this->include('partials/footer');?>
 
 </body>
 </html>
