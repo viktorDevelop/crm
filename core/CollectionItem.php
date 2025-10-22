86683329cb97c083b5ecd195dad56bb50b0cf5e8
Ваше Имя <you@example.com>
2025-10-22 13:43:59 +0300

    коллекция для дельной

--- core/CollectionItem.php
+++ core/CollectionItem.php
@@ -3,5 +3,28 @@ namespace core;
 
 class CollectionItem
 {
+    public function execute($params,$request)
+    {
+        $params = json_decode($params);
+
+        $view = View::getInstance();
+
+        if (!$params->template)
+            return  false;
+
+        if (!$params->model)
+            return false;
+
+        if (!$params->action)
+            return false;
+
+        $obj = new $params->model();
+
+        return $view->render($params->template,[
+            'title'=>$params->title,
+            'arData'=> $obj::{$params->action}($request),
+            'setting_template'=>$params
+        ]);
+    }
 
 }
